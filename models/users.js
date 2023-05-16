02c72eae01315a07af047e369ac73524ae2cea1c
Ksenya Nemkina <xenya.nemkina@yandex.ru>
2023-05-16 12:57:03 +0300

    fixed: 14 пр

--- models/users.js
+++ models/users.js
@@ -18,8 +18,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: (url) => validator.isEmail(url),
-      message: 'Неправильный формат ссылки',
+      validator: (url) => /^(http|https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$/im.test(url),
+      message: 'Поле "avatar" должно быть валидным url-адресом.',
     },
   },
   email: {
