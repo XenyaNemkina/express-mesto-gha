95b66c8178ed00fa39824a4b89ea40794c424447
Ksenya Nemkina <xenya.nemkina@yandex.ru>
2023-05-20 20:19:32 +0300

    fixed: 14 пр 1 ревью

--- models/users.js
+++ models/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../errors/UnautorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -43,12 +44,12 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new UnauthorizedError());
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new UnauthorizedError());
           }
           return user;
         });
